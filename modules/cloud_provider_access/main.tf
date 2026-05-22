741de6512baa041ca9f74b7d528547cbffe76e01
Espen Albert <EspenAlbert@users.noreply.github.com>
2026-05-22 09:23:32 +0100

    feat: Adds configurable timeouts (#38)
    * feat: Adds support for timeouts
    
    * choe: inor changes
    
    * fix(timeouts): Omit GCS bucket read timeout instead of mapping from update
    
    * chore: Updates the snapshots
    
    * address pr comments

--- modules/cloud_provider_access/main.tf
+++ modules/cloud_provider_access/main.tf
@@ -1,6 +1,13 @@
 resource "mongodbatlas_cloud_provider_access_setup" "this" {
   project_id    = var.project_id
   provider_name = "GCP"
+
+  dynamic "timeouts" {
+    for_each = var.timeouts[*]
+    content {
+      create = timeouts.value.create
+    }
+  }
 }
 
 resource "mongodbatlas_cloud_provider_access_authorization" "this" {
